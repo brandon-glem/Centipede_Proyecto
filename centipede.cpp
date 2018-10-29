c1f017e8c9de62802975f80539d39f61d729008e
brandon-glem <brandon.diaz.rivera@gmail.com>
2018-10-29 11:24:10 -0500

    Clase Nave

--- centipede.cpp
+++ centipede.cpp
@@ -1,13 +1,10 @@
 #include <allegro.h>
-//#include "nave.h"
+#include "nave.h"
 #include "disparos.h"
 
 //PANTALLA
 #define ANCHO 672
 #define ALTO 672
-
-const int maxdisp = 4; 
-int ndisp = 0;
 
 void init();
 void deinit();
@@ -16,45 +13,19 @@ int main() {
 	init();
 	//IMAGENES
 	BITMAP *buffer = create_bitmap(ANCHO,ALTO); //PANTALLA NEGRA
-	BITMAP *nave = load_bitmap("level_1/nave-0.bmp",NULL);
-	BITMAP *disparo = load_bitmap("level_1/disparo.bmp",NULL);
-	//COORDENADAS DE LA NAVE
-	int X = ANCHO/2;
-	int Y = ALTO - 50;
-	//CANTIDAD DE DISPAROS
-	const int maxdisparos = 2;
-    int ndisparos = 0;	//DISPAROS REALIZADOS
     
-    Balas disparos[maxdisparos]; //LISTA DE DISPAROS
+    nave n("level_1/nave-0.bmp","level_1/disparo.bmp", 6, 20);
     
-    int dws=0, contt = 0;  //TEMPORIZADOR
+    Balas disparos[n.max_disp]; //LISTA DE DISPAROS
   
 	while(!key[KEY_ESC]) {
           
           clear_to_color(buffer,0x000000); //COLOR
-          masked_blit(nave,buffer,0,0,X,Y,28,40);  //LUGAR DE MOVIMIENTO, TAMAÑO DE LA NAVE
           
-          if(key[KEY_D]){
-                             crear_bala(ndisparos, maxdisparos, disparos, X, Y, -40);//X E Y POSICION NAVE, VELOCIDAD
-                             dws=1;
-                             }
-          if(contt++ > 6){ //OJOOOO
-                     dws = 0;
-                     contt = 0; 
-                     }
-                     
-          pintar_bala(ndisparos, maxdisparos, disparos, buffer, disparo);
-          elimina_bala(ndisparos, maxdisparos, disparos, ANCHO, ALTO);
-	      
-	      if(key[KEY_LEFT])
-                           X -= 8;
-	      if(key[KEY_RIGHT])
-                            X += 8;
-	      if(key[KEY_UP])
-	                        Y -= 8;
-          if(key[KEY_DOWN])
-                            Y += 8;
-	      
+          n.pinta_nave(buffer);
+          n.movimiento();
+          
+          n.dispara(disparos, buffer); 
 	      
 	      blit(buffer,screen, 0, 0, 0, 0, ANCHO, ALTO); //PANTALLA NEGRA 
 	      rest(20);
