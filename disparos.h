e0ec2c63257cc5886241904f04077f36c39f92c2
brandon-glem <brandon.diaz.rivera@gmail.com>
2018-10-29 11:25:16 -0500

    sin modificar

--- disparos.h
+++ disparos.h
@@ -5,13 +5,11 @@
 struct Balas{
     int x , y;
     int dx , dy;
-
 };
 
 void crear_bala(int& n_disparos, const int max_disparos ,struct Balas disparos[] ,
-                const int X, const int Y , const int dy)
-{
-
+                const int X, const int Y , const int dy){
+                      
               if( n_disparos < max_disparos-1){ // n -1 disparos permitidos
                    n_disparos++;
 
@@ -20,27 +18,23 @@ void crear_bala(int& n_disparos, const int max_disparos ,struct Balas disparos[]
                    disparos[n_disparos].dx = 0;
                    disparos[n_disparos].dy = dy;
                }
-
 }
 
 void pintar_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
-                 BITMAP* buffer, BITMAP* bala)
-{
-
+                 BITMAP* buffer, BITMAP* bala, int ancho_b, int alto_b){
+                         
      if ( n_disparos > 0 && n_disparos < max_disparos){
             for ( int cont = 1; cont <= n_disparos; cont++){
                        disparos[cont].x += disparos[cont].dx;
                        disparos[cont].y += disparos[cont].dy;
-                       masked_blit( bala, buffer, 0, 0, disparos[cont].x, disparos[cont].y, 6, 20);
+                       masked_blit( bala, buffer, 0, 0, disparos[cont].x, disparos[cont].y, ancho_b, alto_b);
             }
      }
-
-};
-
+}
 
 void elimina_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
                   const int ANCHO, const int ALTO){
-
+                        
       Balas Btemp;
       if ( n_disparos > 0 && n_disparos < max_disparos){
             for ( int cont = 1; cont <= n_disparos; cont++){
@@ -56,7 +50,6 @@ void elimina_bala(int& n_disparos, const int max_disparos,struct Balas disparos[
             }
       }
 
-
-};
+}
 
 #endif // DISPAROS_H_INCLUDED
