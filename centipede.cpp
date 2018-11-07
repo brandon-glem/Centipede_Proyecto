4b1482e2c29b29158b62c4d6de693de79411472e
YahairaGomez <42581200+YahairaGomez@users.noreply.github.com>
2018-11-06 21:31:48 -0500

    Add files via upload

--- centipede.cpp
+++ centipede.cpp
@@ -1,33 +1,20 @@
 #include <allegro.h>
 #include "nave.h"
 #include "disparos.h"
-#include "diseno.h"
-//#include "spider.h"
 
 //PANTALLA
-#define ANCHO 680
-#define ALTO 748
-
-BITMAP cero;     BITMAP uno;     BITMAP dos;     BITMAP tres;     BITMAP cuatro;
-BITMAP cinco;     BITMAP seis;     BITMAP siete;     BITMAP ocho;     BITMAP nueve;
-
+#define ANCHO 672
+#define ALTO 672
 
 void init();
 void deinit();
 
-
-int main(){
+int main() {
 	init();
 	//IMAGENES
 	BITMAP *buffer = create_bitmap(ANCHO,ALTO); //PANTALLA NEGRA
-    BITMAP *portada = load_bitmap("Recursos/portada.bmp", NULL); 
-    BITMAP *fondo = load_bitmap("Recursos/fondo.bmp", NULL);
     
-    PORTADA(portada);
-    
-    nave n("level_1/nave-1.bmp","level_1/disparo.bmp", 28, 32, 6, 20);
-
-    
+    nave n("level_1/nave-0.bmp","level_1/disparo.bmp", 6, 20, 32,32);
     Balas disparos[n.max_disp]; //LISTA DE DISPAROS
   
 	while(!key[KEY_ESC]) {
@@ -38,8 +25,6 @@ int main(){
           n.movimiento();
           
           n.dispara(disparos, buffer); 
-	      
-	      imprimir_fondo(fondo, buffer);
 	      
 	      blit(buffer,screen, 0, 0, 0, 0, ANCHO, ALTO); //PANTALLA NEGRA 
 	      rest(20);
@@ -56,7 +41,7 @@ void init() {
 	depth = desktop_color_depth();
 	if (depth == 0) depth = 32;
 	set_color_depth(depth);
-	res = set_gfx_mode(GFX_AUTODETECT_WINDOWED, ANCHO, ALTO, 0, 0);
+	res = set_gfx_mode(GFX_AUTODETECT_WINDOWED, 672, 672, 0, 0);
 	if (res != 0) {
 		allegro_message(allegro_error);
 		exit(-1);
--- nave.h
+++ nave.h
@@ -1,13 +1,11 @@
-// 18 columnas y filas de hongos
-
 #include <allegro.h>
 #ifndef NAVE_H_INCLUDED
 #define NAVE_H_INCLUDED
 
 #include "disparos.h"
 
-#define ANCHO 680
-#define ALTO 748
+#define ANCHO 672
+#define ALTO 672
 
 class nave
 {
@@ -20,15 +18,14 @@ class nave
               int max_disp;
               int tick;
               
-              int alto_nave;
-              int ancho_nave;
-              
               int ancho_bala;
               int alto_bala;
-              
+              int ancho_p; //enemigos
+              int alto_p;
               //int vidas=3;
             
-              nave(char *ruta_nave, char *ruta_disparo,int ancho_n, int alto_n, int ancho_b, int alto_b);
+              void nave(char *ruta_nave, char *ruta_disparo, int ancho_b, int alto_b, 
+              int _ancho_p, int _alto_p , int x_, int_y);
              
               void pinta_nave(BITMAP *buffer);
               
@@ -41,10 +38,10 @@ class nave
 
 #endif // NAVE_H
 
-nave::nave(char *ruta_nave, char *ruta_disparo,int ancho_n, int alto_n, int ancho_b, int alto_b){
+nave::nave(char *ruta_nave, char *ruta_disparo, int ancho_b, int alto_b){
                 //COORDENADAS DE LA NAVE
-                x = ANCHO/2;
-                y = ALTO-90;
+                x = _x;
+                y = _y;
                 //CANTIDAD DE DISPAROS
                 max_disp = 5;
                 n_disp = 0; //DISPAROS REALIZADOS
@@ -54,15 +51,14 @@ nave::nave(char *ruta_nave, char *ruta_disparo,int ancho_n, int alto_n, int anch
                 
                 tick = 0;
                 
-                ancho_nave = ancho_n;
-                alto_nave = alto_n;
-                
                 ancho_bala = ancho_b;
                 alto_bala = alto_b;
+                ancho_p= _ancho_p;
+                alto_p= _alto_p;
                 }
                 
 void nave::pinta_nave(BITMAP *buffer){
-     masked_blit(img_nave,buffer,0,0,x,y,ancho_nave,alto_nave);  //LUGAR DE MOVIMIENTO, TAMAÑO DE LA NAVE
+     masked_blit(img_nave,buffer,0,0,x,y,28,40);  //LUGAR DE MOVIMIENTO, TAMAÑO DE LA NAVE
      }
 
 void nave::movimiento(){
@@ -75,6 +71,13 @@ void nave::movimiento(){
      if(key[KEY_DOWN])
                       y += 8;
      }
+void nave::dispara(struct Balas disparos[], BITMAP *buffer){
+     if(key[KEY_D] && temporizador())
+                   crear_bala(n_disp, max_disp, disparos, x, y, -40);//X E Y POSICION NAVE, VELOCIDAD
+                     
+     pintar_bala(n_disp, max_disp, disparos, buffer, img_disp, ancho_bala, alto_bala);
+     elimina_bala(n_disp, max_disp, disparos, ANCHO, ALTO);
+     }
 
 bool nave::temporizador(){
      tick++;
@@ -83,11 +86,4 @@ bool nave::temporizador(){
              return true;
              }
      return false;
-     }
-void nave::dispara(struct Balas disparos[], BITMAP *buffer){
-     if(key[KEY_D] && temporizador())
-                   crear_bala(n_disp, max_disp, disparos, x, y, -40);//X E Y POSICION NAVE, VELOCIDAD
-                     
-     pintar_bala(n_disp, max_disp, disparos, buffer, img_disp, ancho_bala, alto_bala);
-     elimina_bala(n_disp, max_disp, disparos, ANCHO, ALTO);
      }
