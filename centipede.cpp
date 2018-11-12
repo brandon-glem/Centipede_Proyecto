979727473fad51f58eb6dc2ff5ff5c0793f04a46
brandon-glem <brandon.diaz.rivera@gmail.com>
2018-11-12 10:40:56 -0500

    mapa

--- centipede.cpp
+++ centipede.cpp
@@ -1,8 +1,11 @@
 #include <allegro.h>
+#include <stdlib.h>
+#include <time.h>
 #include "nave.h"
 #include "disparos.h"
 #include "diseno.h"
-//#include "spider.h"
+#include "cucaracha.h"
+#include "mapa.h"
 
 //PANTALLA
 #define ANCHO 680
@@ -15,6 +18,16 @@ BITMAP cinco;     BITMAP seis;     BITMAP siete;     BITMAP ocho;     BITMAP nue
 void init();
 void deinit();
 
+int num;
+
+int ale(int &num){
+    srand(time(NULL));
+
+    num = 1 + rand() % (4 - 1);
+               
+    return num;
+}
+
 
 int main(){
 	init();
@@ -22,21 +35,31 @@ int main(){
 	BITMAP *buffer = create_bitmap(ANCHO,ALTO); //PANTALLA NEGRA
     BITMAP *portada = load_bitmap("Recursos/portada.bmp", NULL); 
     BITMAP *fondo = load_bitmap("Recursos/fondo.bmp", NULL);
+    BITMAP *hongo = load_bitmap("level_1/hongo-1.bmp",NULL);
     
     PORTADA(portada);
     
-    nave n("level_1/nave-1.bmp","level_1/disparo.bmp", 28, 32, 6, 20);
+    nave n("level_1/nave-1.bmp","level_1/disparo.bmp", 21, 24, 6, 20, ANCHO/2, ALTO-90);
+    cucaracha c("level_1/pulga-1.bmp","level_1/hongo-1.bmp",27,24,24,24);
 
     
     Balas disparos[n.max_disp]; //LISTA DE DISPAROS
   
+    mapa HO[65];
+    iniciar_mapa(HO); 
+  
 	while(!key[KEY_ESC]) {
           
           clear_to_color(buffer,0x000000); //COLOR
+          pinta_mapa(HO, hongo, buffer);
           
           n.pinta_nave(buffer);
           n.movimiento();
-          
+          if(ale(num) == 2){
+                         c.pintar_cucaracha(buffer);
+                         c.movimiento(buffer);
+                         }
+                         
           n.dispara(disparos, buffer); 
 	      
 	      imprimir_fondo(fondo, buffer);
